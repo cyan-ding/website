2c1e4cb73da0033348abde126cf51abbbf71016d
Cyan Ding <blueplus.d@gmail.com>
2025-11-07 21:07:04 -0800

    update resume

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <h1>Hey! I'm Cyan. I just started this website</h1>
 <object 
-  data="/resume_Cyan_berkeley_tech.pdf" 
+  data="/resume_Cyan_website.pdf" 
   type="application/pdf" 
   width="100%" 
   height="775px"
 >
   <p>Your browser doesn't support PDF viewing. 
-    <a href="/resume_Cyan_berkeley_tech.pdf">Download the PDF</a>
+    <a href="/resume_Cyan_website.pdf">Download the PDF</a>
   </p>
 </object>
